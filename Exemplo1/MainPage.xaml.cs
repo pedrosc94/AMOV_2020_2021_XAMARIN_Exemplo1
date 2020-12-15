74d1ae7d90853cff26c862f54a50de8cdc24b089
Pedro Carvalho <pedrosilvacar@gmail.com>
2020-12-15 17:55:09 +0000

    First Commit

---
+++ Exemplo1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Class:
+ *      AMOV 2020/2021
+ *      
+ * Project:
+ *      Xamarim Exemplo 1
+ * 
+ * Authors:
+ *      Andre Mendes
+ *      Pedro Carvalho
+ *      Vasco Patricio
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exemplo1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //Button Event
+        private void ButtonTextInput_Clicked(object sender, EventArgs e)
+        {
+            editorTextOutput.Text = editorTextInput.Text;
+        }
+
+    }
+}
